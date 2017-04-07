162fd37b1ec3fea8abd5d26225ebdbcdf400fddc
Josh Yee <j-yee@hotmail.com.au>
2017-04-07 17:02:48 +1000

    Added logo and made few adjustments to layout styles

--- B.E.T.T.E.R/Member.Master.designer.cs
+++ B.E.T.T.E.R/Member.Master.designer.cs
@@ -22,6 +22,15 @@ namespace B.E.T.T.E.R {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// logo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image logo;
+        
+        /// <summary>
         /// form1 control.
         /// </summary>
         /// <remarks>
--- B.E.T.T.E.R/Properties/Resources.Designer.cs
+++ B.E.T.T.E.R/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace B.E.T.T.E.R.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap logo {
+            get {
+                object obj = ResourceManager.GetObject("logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
