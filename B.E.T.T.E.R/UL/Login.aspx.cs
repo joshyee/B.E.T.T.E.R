f8f48f63ef08f5a1aefa9a745dae659bd06fe2f1
joshyee <j-yee@hotmail.com.au>
2017-08-17 19:32:20 +1000

    Added code-behind the login page with dummy username and password which is stored in a session variable

--- B.E.T.T.E.R/UL/Login.aspx.cs
+++ B.E.T.T.E.R/UL/Login.aspx.cs
@@ -18,38 +18,16 @@ namespace B.E.T.T.E.R.UL
 
         protected void btnLogin_Click(object sender, EventArgs e)
         {
-            // Check if user exists in database
-            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["udbBetterConnectionString"].ConnectionString);
-            conn.Open();
-            string checkUser = "select count(*) from tblUser where username =  '" + txtUsername.Text + "'";
-            SqlCommand com = new SqlCommand(checkUser, conn);
-            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
-            conn.Close();
-            if (temp == 1)
+            // Check if login details are correct
+            if (txtUsername.Text == "test" && txtPwd.Text == "better3050")
             {
-                conn.Open();
-                string checkPasswordQuery = "select passcode from tblUser where username =  '" + txtUsername.Text + "'";
-                SqlCommand passComm = new SqlCommand(checkPasswordQuery, conn);
-                string password = passComm.ExecuteScalar().ToString().Replace(" ", "");
-                if (password == txtPwd.Text)
-                {
-                    // If username and password is correct, start session and redirect member to the main menu
-                    Session["user"] = txtUsername.Text;
-                    Response.Redirect("MainMenu.aspx");
-                }
-                else
-                {
-                    // Incorrect username or password
-                    lblError.Visible = true;
-                }
-               
+                Session["user"] = Convert.ToString(txtUsername.Text);
+                Response.Redirect("MainMenu.aspx");
             }
             else
             {
-                // Incorrect username or password
                 lblError.Visible = true;
             }
-
         }
     }
 }
--- B.E.T.T.E.R/UL/MainMenu.aspx.cs
+++ B.E.T.T.E.R/UL/MainMenu.aspx.cs
@@ -13,38 +13,7 @@ namespace B.E.T.T.E.R.UL
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Open new connection and find user in the database
-            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["udbBetterConnectionString"].ConnectionString);
             
-            // Get userId from the database
-            conn.Open();
-            string checkUserId = "select userId from tblUser where username =  '" + (string)Session["user"] + "'";
-            SqlCommand userComm = new SqlCommand(checkUserId, conn);
-            string userId = userComm.ExecuteScalar().ToString().Replace(" ", "");
-            Session["userId"] = userId;
-            conn.Close();
-            
-            conn.Open();
-            string checkUser = "select count(*) from tblUser where username =  '" + (string)Session["user"] + "'";
-            SqlCommand com = new SqlCommand(checkUser, conn);
-            int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
-            conn.Close();
-            if (temp == 1)
-            {
-                // Display welcome message
-                lblUsername.Text = "Welcome back, " + (string)Session["user"];
-
-                // Get the user's exercise points balance from the database
-                conn.Open();
-                string checkPointsQuery = "select exercisePoints from tblUser where username =  '" + (string)Session["user"] + "'";
-                SqlCommand pointsComm = new SqlCommand(checkPointsQuery, conn);
-                string points = pointsComm.ExecuteScalar().ToString().Replace(" ", "");
-
-                lblExercisePoints.Text = points + " XP";
-                conn.Close();
-            }
-          
-  
         }
 
         protected void btnManagePoints_Click(object sender, EventArgs e)
